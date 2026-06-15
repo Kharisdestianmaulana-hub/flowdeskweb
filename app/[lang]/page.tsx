6e3887c9737f792cb480dbc40a224d174cc57ddc
Kharis <maulanakharis129@gmail.com>
2026-06-15 16:36:18 +0700

    chore: revert parallax stacking effect as requested

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -13,7 +13,6 @@ import HowItWorks from '@/components/HowItWorks';
 import FAQ from '@/components/FAQ';
 import Newsletter from '@/components/Newsletter';
 import Contributors from '@/components/Contributors';
-import StackedCard from '@/components/StackedCard';
 
 import { getRepoInfo, getLatestRelease, getRecentCommits, getContributors } from '@/lib/github';
 import { getDictionary } from '@/lib/dictionary';
@@ -41,56 +40,37 @@ export default async function Home({ params }: { params: Promise<{ lang: string 
         dict={dict.hero}
       />
       
-      {/* Stacking Parallax Wrapper */}
-      <div className="relative z-10">
-        <StackedCard zIndex={10} bgClass="bg-[var(--color-surface)] border-t border-[var(--color-border-subtle)]">
-          <AppPreview dict={dict.appPreview} />
-        </StackedCard>
+      <AppPreview dict={dict.appPreview} />
 
-        <StackedCard zIndex={20} bgClass="bg-[var(--color-bg)]">
-          <div className="flex flex-col gap-0">
-            <SocialProof dict={dict.socialProof} />
-            <Features dict={dict.features} />
-          </div>
-        </StackedCard>
-
-        <StackedCard zIndex={30} bgClass="bg-[var(--color-surface)]">
-          <UseCases dict={dict.useCases} />
-        </StackedCard>
-        
-        <StackedCard zIndex={40} bgClass="bg-[var(--color-bg)]">
-          <WhyFlowDesk dict={dict.whyFlowDesk} />
-        </StackedCard>
-
-        <StackedCard zIndex={50} bgClass="bg-[var(--color-surface)]">
-          <HowItWorks dict={dict.howItWorks} />
-        </StackedCard>
-        
-        <StackedCard zIndex={60} bgClass="bg-[var(--color-bg)]" isLast={true}>
-          <PlatformDownload 
-            assets={latestRelease.assets} 
-            version={latestRelease.tag_name} 
-            dict={dict.download}
-          />
-        </StackedCard>
-      </div>
+      <SocialProof dict={dict.socialProof} />
       
-      {/* Normal Scrolling Sections */}
-      <div className="relative z-[70] bg-[var(--color-bg)] shadow-[0_-20px_50px_rgba(0,0,0,0.5)]">
-        <ChangelogStrip commits={commits} dict={dict.changelogStrip} currentLang={lang} />
-        
-        <FAQ dict={dict.faq} />
+      <Features dict={dict.features} />
 
-        <Contributors contributors={contributors} lang={lang} />
+      <UseCases dict={dict.useCases} />
+      
+      <WhyFlowDesk dict={dict.whyFlowDesk} />
 
-        <CTASection assets={latestRelease.assets} dict={dict.cta} />
-        
-        <div className="pb-24 px-4 sm:px-6 lg:px-8">
-          <Newsletter dict={dict.newsletter} />
-        </div>
+      <HowItWorks dict={dict.howItWorks} />
+      
+      <PlatformDownload 
+        assets={latestRelease.assets} 
+        version={latestRelease.tag_name} 
+        dict={dict.download}
+      />
+      
+      <ChangelogStrip commits={commits} dict={dict.changelogStrip} currentLang={lang} />
+      
+      <FAQ dict={dict.faq} />
 
-        <Footer repoUrl={repoInfo.html_url} dict={dict.footer} currentLang={lang} />
+      <Contributors contributors={contributors} lang={lang} />
+
+      <CTASection assets={latestRelease.assets} dict={dict.cta} />
+      
+      <div className="pb-24 px-4 sm:px-6 lg:px-8">
+        <Newsletter dict={dict.newsletter} />
       </div>
+
+      <Footer repoUrl={repoInfo.html_url} dict={dict.footer} currentLang={lang} />
     </main>
   );
 }
