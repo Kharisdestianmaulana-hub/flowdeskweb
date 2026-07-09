4311bd95ba6d2e25764e307119debfec6d537fde
Kharis <maulanakharis129@gmail.com>
2026-07-09 15:15:20 +0700

    style: center blog reader layout horizontally

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -75,7 +75,7 @@ export default async function BlogPostPage({ params }: { params: Promise<{ lang:
       <ProgressBar />
       <Navbar stars={repoInfo.stargazers_count} repoUrl={repoInfo.html_url} dict={dict.navbar} currentLang={lang} />
       
-      <div className="flex-1 w-full max-w-6xl mx-auto px-6 py-24 sm:py-32 flex flex-col lg:flex-row gap-12 xl:gap-24 items-start">
+      <div className="flex-1 w-full max-w-6xl mx-auto px-6 py-24 sm:py-32 flex flex-col lg:flex-row gap-12 xl:gap-24 items-start justify-center">
         
         {/* Main Article Content */}
         <article className="w-full lg:w-[720px] shrink-0">
